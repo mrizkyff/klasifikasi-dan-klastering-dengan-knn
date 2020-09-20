2b5f7ce51e8c6aebafbe5069473fd02799e6049e
mrizkyff <muhamadrizkyff@gmail.com>
2020-09-20 20:11:32 +0700

    sidebar collapse dan hapus script jquery di footer

--- application/views/template/admin/footer.php
+++ application/views/template/admin/footer.php
@@ -59,12 +59,5 @@
 <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
 
-
-
-<script>
-  $(document).ready(function() {
-    $('#example').DataTable();
-} );
-</script>
 </body>
 </html>
