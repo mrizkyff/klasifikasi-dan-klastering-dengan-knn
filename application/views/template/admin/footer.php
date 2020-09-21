5b514c7166bd44e6f2915605ebcdb81443195f2a
mrizkyff <muhamadrizkyff@gmail.com>
2020-09-21 21:35:13 +0700

    update header footer

--- application/views/template/admin/footer.php
+++ application/views/template/admin/footer.php
@@ -3,7 +3,7 @@
     <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.1
+      <b>Version</b> 3.0.5
     </div>
   </footer>
 
@@ -16,48 +16,46 @@
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="<?php echo base_url() ?>asset/plugins/jquery/jquery.min.js"></script>
+<!-- <script src="<?php echo base_url()?>asset/plugins/jquery/jquery.min.js"></script> -->
+<script src="https://code.jquery.com/jquery-3.5.1.js" ></script>
+<!-- Bootstrap 4 -->
+<script src="<?php echo base_url()?>asset/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- DataTables -->
+<script src="<?php echo base_url()?>asset/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?php echo base_url()?>asset/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="<?php echo base_url()?>asset/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="<?php echo base_url()?>asset/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
 <!-- jQuery UI 1.11.4 -->
-<script src="<?php echo base_url() ?>asset/plugins/jquery-ui/jquery-ui.min.js"></script>
+<script src="<?php echo base_url()?>asset/plugins/jquery-ui/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <script>
   $.widget.bridge('uibutton', $.ui.button)
 </script>
 <!-- Bootstrap 4 -->
-<script src="<?php echo base_url() ?>asset/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo base_url()?>asset/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- ChartJS -->
-<script src="<?php echo base_url() ?>asset/plugins/chart.js/Chart.min.js"></script>
+<script src="<?php echo base_url()?>asset/plugins/chart.js/Chart.min.js"></script>
 <!-- Sparkline -->
-<script src="<?php echo base_url() ?>asset/plugins/sparklines/sparkline.js"></script>
+<script src="<?php echo base_url()?>asset/plugins/sparklines/sparkline.js"></script>
 <!-- JQVMap -->
-<script src="<?php echo base_url() ?>asset/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="<?php echo base_url() ?>asset/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="<?php echo base_url()?>asset/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="<?php echo base_url()?>asset/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
 <!-- jQuery Knob Chart -->
-<script src="<?php echo base_url() ?>asset/plugins/jquery-knob/jquery.knob.min.js"></script>
+<script src="<?php echo base_url()?>asset/plugins/jquery-knob/jquery.knob.min.js"></script>
 <!-- daterangepicker -->
-<script src="<?php echo base_url() ?>asset/plugins/moment/moment.min.js"></script>
-<script src="<?php echo base_url() ?>asset/plugins/daterangepicker/daterangepicker.js"></script>
+<script src="<?php echo base_url()?>asset/plugins/moment/moment.min.js"></script>
+<script src="<?php echo base_url()?>asset/plugins/daterangepicker/daterangepicker.js"></script>
 <!-- Tempusdominus Bootstrap 4 -->
-<script src="<?php echo base_url() ?>asset/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="<?php echo base_url()?>asset/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
 <!-- Summernote -->
-<script src="<?php echo base_url() ?>asset/plugins/summernote/summernote-bs4.min.js"></script>
+<script src="<?php echo base_url()?>asset/plugins/summernote/summernote-bs4.min.js"></script>
 <!-- overlayScrollbars -->
-<script src="<?php echo base_url() ?>asset/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<script src="<?php echo base_url()?>asset/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
-<script src="<?php echo base_url() ?>asset/dist/js/adminlte.js"></script>
+<script src="<?php echo base_url()?>asset/dist/js/adminlte.js"></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="<?php echo base_url() ?>asset/dist/js/pages/dashboard.js"></script>
+<script src="<?php echo base_url()?>asset/dist/js/pages/dashboard.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="<?php echo base_url() ?>asset/dist/js/demo.js"></script>
-
-<!-- JS Datatables -->
-<!-- <script src="<?php echo base_url() ?>asset/plugins/datatables/jquery.dataTables.js"></script>
-
-<script src="<?php echo base_url() ?>asset/plugins/datatables-bs4/js/dataTables.bootstrap4.js"></script> -->
-
-<!-- javasccript cdn -->
-<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
-
+<script src="<?php echo base_url()?>asset/dist/js/demo.js"></script>
 </body>
 </html>
--- application/views/template/admin/header.php
+++ application/views/template/admin/header.php
@@ -9,12 +9,11 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>DNRepository IT Department | Dashboard</title>
+  <title>AdminLTE 3 | Dashboard</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?php echo base_url() ?>asset/plugins/fontawesome-freeWEB/css/all.min.css">
-  <!-- <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" /> -->
+  <link rel="stylesheet" href="<?php echo base_url() ?>asset/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bbootstrap 4 -->
@@ -25,11 +24,6 @@
   <link rel="stylesheet" href="<?php echo base_url() ?>asset/plugins/jqvmap/jqvmap.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="<?php echo base_url() ?>asset/dist/css/adminlte.min.css">
-  <!-- Data table -->
-  <!-- <link rel="stylesheet" href="<?php echo base_url() ?>asset/plugins/datatables-bs4/css/dataTables.bootstrap4.css"> -->
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
-  
-  
   <!-- overlayScrollbars -->
   <link rel="stylesheet" href="<?php echo base_url() ?>asset/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Daterange picker -->
@@ -38,6 +32,9 @@
   <link rel="stylesheet" href="<?php echo base_url() ?>asset/plugins/summernote/summernote-bs4.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <!-- halo cdn -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bootstrap4.min.css"/>
 </head>
 <body class="hold-transition sidebar-mini layout-fixed sidebar-collapse">
 <div class="wrapper">
