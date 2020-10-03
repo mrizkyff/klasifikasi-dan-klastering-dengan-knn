e14eba9e8426545c7266510066717a9b9ec64ae0
mrizkyff <muhamadrizkyff@gmail.com>
2020-10-03 19:40:05 +0700

    perbaikan halaman not found

--- application/controllers/NotFound.php
+++ application/controllers/NotFound.php
@@ -4,9 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class NotFound extends CI_Controller
 {
     public function index(){
-        $this->load->view('template/s_header');
+        $this->load->view('template/public/pub_header');
         $this->load->view('notfound');
-        $this->load->view('template/s_footer');
+        $this->load->view('template/public/pub_footer');
     }
 }
 
