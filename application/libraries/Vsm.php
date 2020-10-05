9a4735a2c597a4b22891e368beb62f9fe59c8cea
mrizkyff <muhamadrizkyff@gmail.com>
2020-10-05 23:12:56 +0700

    simplify cosim, jaccard dan dice jadi hanya 1 fungsi (ikut dengan cosim)

--- application/libraries/Vsm.php
+++ application/libraries/Vsm.php
@@ -20,12 +20,12 @@ class VSM
         $df             = VSM::df($term, $query, $dokumen_term, $debug);
         $idf            = VSM::idf($query, $dokumen_term, $df, $debug);
         $bobot          = VSM::bobot($query, $dokumen_term, $idf, $debug);
-        // $cos_similarity = VSM::cosine_similarity($bobot, $debug);
+        $cos_similarity = VSM::cosine_similarity($bobot, $debug);
         // $jaccard_similarity = VSM::jaccard_similarity($bobot, $debug);
-        $dice_similarity = VSM::dice_similarity($bobot, $debug);
+        // $dice_similarity = VSM::dice_similarity($bobot, $debug);
 
-        // return $cos_similarity;
-        return $dice_similarity;
+        return $cos_similarity;
+        // return $dice_similarity;
     }
 
     /**
@@ -212,12 +212,47 @@ class VSM
 
     /**
      * Melakukan perangking-an dengan cosine similarity
+     * 
+     * Ada Jaccard Juga
+     * intinya perbedaannya dengan cosim adalah ini tu perkalian skalar / (jumlah vektor d1 + jml vektor query - perkalian skalar)
+     * 
+     * Ada Dice Juga
+     * dice itu intinya hampir sama dengan jaccard, tapi dikali 2 dan tidak dikurangi perkalian skalar
      *
      * @param  array $bobot
      * @return array
     */
-    public static function cosine_similarity($bobot, $debug)
+    public static function cosine_similarity($dis, $debug)
     {
+        // ====================================================================================
+        $bobot = [];
+        $bobot['query'] = [
+            'x' => 1.5,
+            'y' => 1.0,
+            'z' => 0.0,
+        ];
+        $bobot['dokumen'] = [
+            '0' => [
+                'id_doc' => 'g1',
+                'dokumen' => [
+                    'x' => 0.5,
+                    'y' => 0.8,
+                    'z' => 0.3,
+                ],
+            ],
+            '1' => [
+                'id_doc' => 'g2',
+                'dokumen' => [
+                    'x' => 0.9,
+                    'y' => 0.4,
+                    'z' => 0.2,
+                ],
+            ],
+        ];
+        // print_r($dis);
+        // print_r($bobot);
+        // ====================================================================================
+        
         //edit ian
         // mendapatkan jumlah dan akar dari query @float
         $queryCosJumlah = 0;
@@ -275,11 +310,16 @@ class VSM
 
         // mendapatkan besar vektor
         $dokumenBesarVektor = [];
+        $dokumenBesarVektorJaccard = [];
         foreach ($dokumenCosJumlahAkar as $index => $dokumen) {
             $arrayDoc[$index] = ["id_doc" => $dokumen['id_doc']];
             $besarVektorDoc[$index] = $dokumen['akar_bobot'];
 
             $arrayDoc[$index] += ["jumlah_besar_vektor" => ($besarVektorDoc[$index] * $queryCosAkar) ];
+            
+            // FLAG = JACCARD
+            // menambahkan jumlah vektor dokumen dengan jumlah vektor query (dimasukkan ke arraydoc tapi ada array jaccard didalamnya)
+            $arrayDoc[$index] += ["jumlah_besar_vektor_jaccard" => $dokumen['jumlah_bobot'] + $queryCosJumlah ];
             array_push($dokumenBesarVektor,  $arrayDoc[$index]);
         }
         // dd($dokumenVektor, $dokumenBesarVektor);
@@ -294,8 +334,11 @@ class VSM
             foreach ($dokumenBesarVektor as $index1 => $dokumen1) {
                 if ($dokumen1['id_doc'] == $dokumen['id_doc']) {
                     $vectorCosine[$index] = $dokumen1["jumlah_besar_vektor"];
-
                     $arrayDoc[$index] += ["jumlah_besar_vektor" => $vectorCosine[$index] ];
+                    
+                    // FLAG = JACCARD
+                    $vectorCosineJaccard[$index] = $dokumen1["jumlah_besar_vektor_jaccard"];
+                    $arrayDoc[$index] += ["jumlah_besar_vektor_jaccard" => $vectorCosineJaccard[$index] ];
                     array_push($dokumenCosine,  $arrayDoc[$index]);
                 }
             }
@@ -307,19 +350,31 @@ class VSM
 
         // membuat ranking
         $dokumenRanking = [];
+        $dokumenRankingJaccard = [];
+        $dokumenRankingDice = [];
         foreach ($dokumenCosine as $index => $dokumen) {
             $jumlah = 0;
             if ($dokumen["jumlah_vektor"] != 0 && $dokumen["jumlah_besar_vektor"] != 0) {
                 $jumlah = $dokumen["jumlah_vektor"] / $dokumen["jumlah_besar_vektor"];
+
+                // FLAG = JACCARD
+                $jumlahJaccard = $dokumen['jumlah_vektor'] / ($dokumen['jumlah_besar_vektor_jaccard']-$dokumen['jumlah_vektor']);
+
+                // FLAG = DICE
+                $jumlahDice = 2*($dokumen['jumlah_vektor']) / ($dokumen['jumlah_besar_vektor_jaccard']);
             }
             $arrayDoc[$index] = ["id_doc" => $dokumen['id_doc'], "ranking" => $jumlah];
+            $arrayDocJaccard[$index] = ["id_doc" => $dokumen['id_doc'], "ranking" => $jumlahJaccard];
+            $arrayDocDice[$index] = ["id_doc" => $dokumen['id_doc'], "ranking" => $jumlahDice];
             array_push($dokumenRanking, $arrayDoc[$index]);
+            array_push($dokumenRankingJaccard, $arrayDocJaccard[$index]);
+            array_push($dokumenRankingDice, $arrayDocDice[$index]);
         }
 
         if ($debug){
-            var_dump('============== COSINUS SIMILARITY ==============');
+            var_dump('============== COSINUS SIMILARITY  dan JACCARD ==============');
             var_dump('-------- jumlah dan akar query --------');
-            print_r($queryCosAkar);
+            print_r(['jumlah' => $queryCosJumlah, 'akar' => $queryCosAkar]);
             var_dump('/n-------- jumlah dan akar dokumen --------');
             print_r($dokumenCosJumlahAkar);
             var_dump('-------- vektor dokumen (perkalian)(pembilang)--------');
@@ -330,320 +385,12 @@ class VSM
             print_r($dokumenCosine);
             var_dump('-------- rangking dokumen (hasil) --------');
             print_r($dokumenRanking);
+            print_r($dokumenRankingJaccard);
+            print_r($dokumenRankingDice);
         }
 
         return $dokumenRanking;
     }
-    
-    /**
-     * 
-     * Perhitungan jaccard similarity
-     * intinya perbedaannya dengan cosim adalah ini tu perkalian skalar / (jumlah vektor d1 + jml vektor query - perkalian skalar)
-     * @param array @bobot
-     * @param $debug
-     */
-    public static function jaccard_similarity($dis, $debug)
-    {
-
-        // ====================================================================================
-        $bobot = [];
-        $bobot['query'] = [
-            'x' => 1.5,
-            'y' => 1.0,
-            'z' => 0.0,
-        ];
-        $bobot['dokumen'] = [
-            '0' => [
-                'id_doc' => 'g1',
-                'dokumen' => [
-                    'x' => 0.5,
-                    'y' => 0.8,
-                    'z' => 0.3,
-                ],
-            ],
-            '1' => [
-                'id_doc' => 'g2',
-                'dokumen' => [
-                    'x' => 0.9,
-                    'y' => 0.4,
-                    'z' => 0.2,
-                ],
-            ],
-        ];
-        // print_r($dis);
-        // print_r($bobot);
-        // ====================================================================================
-
-        //edit ian
-        // mendapatkan jumlah dan akar dari query @float
-        // yang dipakai hanya jumlah query
-        $queryCosJumlah = 0;
-        foreach ($bobot['query'] as $key => $value) {
-            $queryCosJumlah += $value*$value;
-        }
-        $queryCosAkar = sqrt($queryCosJumlah);
-        // print_r(['jml' => $queryCosJumlah]);
-        // die();
-        
-        //edit ian
-        // mendapatkan bobot dokumen
-        // mendapatkan jumlah dan akar dari setiap dokumen @array
-        // jadi sebelumnya itu pembobotan dokumen harus mencari term di query dan dokumen yang sama baru dijumlahkan, tapi aku ganti langsung aja jumlahin semua bobot yang ada di sebuah dokumen
-        $dokumenCosJumlahAkar = [];
-        foreach ($bobot['dokumen'] as $index => $dokumen) { // dokumen 1, 2, 3 ... n
-            $arrayDoc[$index] = ["id_doc" => $dokumen['id_doc']];
-            $bobotDoc[$index] = 0;
-            // foreach ($bobot['query'] as $key => $value) {
-                foreach ($dokumen['dokumen'] as $key1 => $value1) { // isi dr dokumen 1, 2, ..n
-                    // var_dump($key1);
-                    // if ($key == $key1) {
-                        $value1 = ($value1*$value1);
-                        // var_dump($value1);
-                        $bobotDoc[$index] += ($value1);
-                        // var_dump($bobotDoc);
-                    // }
-                }
-            // }
-            $arrayDoc[$index] += ["jumlah_bobot" => $bobotDoc[$index], "akar_bobot" => sqrt($bobotDoc[$index])];
-            array_push($dokumenCosJumlahAkar,  $arrayDoc[$index]);   
-        }
-        
-        
-
-        // mendapatkan vektor
-        // jadi ketika term pada query dan dokumen itu sama, dikalikan lalu dijumlahkan sehingga menghasilkan jumlah vektor pada masing2 dokumen
-        $dokumenVektor = [];
-        foreach ($bobot['dokumen'] as $index => $dokumen) { // dokumen 1, 2, 3 ... n
-            $arrayDoc[$index] = ["id_doc" => $dokumen['id_doc']];
-            $vektorDoc[$index] = 0;
-            foreach ($bobot['query'] as $key => $value) {
-                foreach ($dokumen['dokumen'] as $key1 => $value1) { // isi dr dokumen 1, 2, ..n
-                    if ($key == $key1) {
-                        $vektorDoc[$index] += ($value * $value1);
-                    }
-                }
-            }
-            $arrayDoc[$index] += ["jumlah_vektor" => $vektorDoc[$index] ];
-            array_push($dokumenVektor,  $arrayDoc[$index]);
-        }
-
-        
-
-        
-        // mendapatkan besar vektor
-        $dokumenBesarVektor = [];
-        foreach ($dokumenCosJumlahAkar as $index => $dokumen) {
-            $arrayDoc[$index] = ["id_doc" => $dokumen['id_doc']];
-            // menambahkan jumlah vektor dokumen dengan jumlah vektor query
-            $arrayDoc[$index] += ["jumlah_besar_vektor" => $dokumen['jumlah_bobot']+ $queryCosJumlah ];
-            array_push($dokumenBesarVektor,  $arrayDoc[$index]);
-        }
-        print_r(['penyebut_kiri' => $dokumenBesarVektor]);
-        // dd($dokumenVektor, $dokumenBesarVektor);
-        
-
-        // gabungkan array vektor dan besar vektor
-        $dokumenCosine = [];
-        foreach ($dokumenVektor as $index => $dokumen) {
-            $arrayDoc[$index] = ["id_doc" => $dokumen['id_doc'], "jumlah_vektor" => $dokumen['jumlah_vektor'] ];
-            $vectorCosine[$index] = 0;
-
-            foreach ($dokumenBesarVektor as $index1 => $dokumen1) {
-                if ($dokumen1['id_doc'] == $dokumen['id_doc']) {
-                    $vectorCosine[$index] = $dokumen1["jumlah_besar_vektor"];
-
-                    $arrayDoc[$index] += ["jumlah_besar_vektor" => $vectorCosine[$index] ];
-                    array_push($dokumenCosine,  $arrayDoc[$index]);
-                }
-            }
-        }
-
-        // membuat ranking
-        $dokumenRanking = [];
-        foreach ($dokumenCosine as $index => $dokumen) {
-            $jumlah = 0;
-            if ($dokumen["jumlah_vektor"] != 0 && $dokumen["jumlah_besar_vektor"] != 0) {
-                // ganti rumus sesuai dengan aturan jaccard
-                $jumlah = $dokumen['jumlah_vektor'] / ($dokumen['jumlah_besar_vektor']-$dokumen['jumlah_vektor']);
-            }
-            $arrayDoc[$index] = ["id_doc" => $dokumen['id_doc'], "ranking" => $jumlah];
-            array_push($dokumenRanking, $arrayDoc[$index]);
-        }
-
-        if ($debug){
-            var_dump('============== JACCARD SIMILARITY ==============');
-
-            var_dump('-------- jumlah query --------');
-            print_r($queryCosJumlah);
-            var_dump('/n-------- jumlah dan akar dokumen --------');
-            print_r($dokumenCosJumlahAkar);
-            var_dump('-------- vektor dokumen (perkalian)(pembilang)--------');
-            print_r($dokumenVektor);
-            var_dump('-------- besar vektor dokumen (penyebut) --------');
-            print_r($dokumenBesarVektor);
-            var_dump('-------- cosinus dokumen --------');
-            print_r($dokumenCosine);
-            var_dump('-------- rangking dokumen (hasil) --------');
-            print_r($dokumenRanking);
-        }
-
-        return $dokumenRanking;
-    }
-    /**
-     * 
-     * Perhitungan dice similarity
-     * intinya perbedaannya dengan cosim adalah ini tu 2x(perkalian skalar) / (jumlah vektor d1 + jml vektor query)
-     * @param array @bobot
-     * @param $debug
-     */
-    public static function dice_similarity($dis, $debug)
-    {
-
-        // ====================================================================================
-        $bobot = [];
-        $bobot['query'] = [
-            'x' => 1.5,
-            'y' => 1.0,
-            'z' => 0.0,
-        ];
-        $bobot['dokumen'] = [
-            '0' => [
-                'id_doc' => 'g1',
-                'dokumen' => [
-                    'x' => 0.5,
-                    'y' => 0.8,
-                    'z' => 0.3,
-                ],
-            ],
-            '1' => [
-                'id_doc' => 'g2',
-                'dokumen' => [
-                    'x' => 0.9,
-                    'y' => 0.4,
-                    'z' => 0.2,
-                ],
-            ],
-        ];
-        // print_r($dis);
-        // print_r($bobot);
-        // ====================================================================================
-
-        //edit ian
-        // mendapatkan jumlah dan akar dari query @float
-        // yang dipakai hanya jumlah query
-        $queryCosJumlah = 0;
-        foreach ($bobot['query'] as $key => $value) {
-            $queryCosJumlah += $value*$value;
-        }
-        $queryCosAkar = sqrt($queryCosJumlah);
-        // print_r(['jml' => $queryCosJumlah]);
-        // die();
-        
-        //edit ian
-        // mendapatkan bobot dokumen
-        // mendapatkan jumlah dan akar dari setiap dokumen @array
-        // jadi sebelumnya itu pembobotan dokumen harus mencari term di query dan dokumen yang sama baru dijumlahkan, tapi aku ganti langsung aja jumlahin semua bobot yang ada di sebuah dokumen
-        $dokumenCosJumlahAkar = [];
-        foreach ($bobot['dokumen'] as $index => $dokumen) { // dokumen 1, 2, 3 ... n
-            $arrayDoc[$index] = ["id_doc" => $dokumen['id_doc']];
-            $bobotDoc[$index] = 0;
-            // foreach ($bobot['query'] as $key => $value) {
-                foreach ($dokumen['dokumen'] as $key1 => $value1) { // isi dr dokumen 1, 2, ..n
-                    // var_dump($key1);
-                    // if ($key == $key1) {
-                        $value1 = ($value1*$value1);
-                        // var_dump($value1);
-                        $bobotDoc[$index] += ($value1);
-                        // var_dump($bobotDoc);
-                    // }
-                }
-            // }
-            $arrayDoc[$index] += ["jumlah_bobot" => $bobotDoc[$index], "akar_bobot" => sqrt($bobotDoc[$index])];
-            array_push($dokumenCosJumlahAkar,  $arrayDoc[$index]);   
-        }
-        
-        
-
-        // mendapatkan vektor
-        // jadi ketika term pada query dan dokumen itu sama, dikalikan lalu dijumlahkan sehingga menghasilkan jumlah vektor pada masing2 dokumen
-        $dokumenVektor = [];
-        foreach ($bobot['dokumen'] as $index => $dokumen) { // dokumen 1, 2, 3 ... n
-            $arrayDoc[$index] = ["id_doc" => $dokumen['id_doc']];
-            $vektorDoc[$index] = 0;
-            foreach ($bobot['query'] as $key => $value) {
-                foreach ($dokumen['dokumen'] as $key1 => $value1) { // isi dr dokumen 1, 2, ..n
-                    if ($key == $key1) {
-                        $vektorDoc[$index] += ($value * $value1);
-                    }
-                }
-            }
-            $arrayDoc[$index] += ["jumlah_vektor" => $vektorDoc[$index] ];
-            array_push($dokumenVektor,  $arrayDoc[$index]);
-        }
-
-        
-
-        
-        // mendapatkan besar vektor
-        $dokumenBesarVektor = [];
-        foreach ($dokumenCosJumlahAkar as $index => $dokumen) {
-            $arrayDoc[$index] = ["id_doc" => $dokumen['id_doc']];
-            // menambahkan jumlah vektor dokumen dengan jumlah vektor query
-            $arrayDoc[$index] += ["jumlah_besar_vektor" => $dokumen['jumlah_bobot']+ $queryCosJumlah ];
-            array_push($dokumenBesarVektor,  $arrayDoc[$index]);
-        }
-        print_r(['penyebut_kiri' => $dokumenBesarVektor]);
-        // dd($dokumenVektor, $dokumenBesarVektor);
-        
-
-        // gabungkan array vektor dan besar vektor
-        $dokumenCosine = [];
-        foreach ($dokumenVektor as $index => $dokumen) {
-            $arrayDoc[$index] = ["id_doc" => $dokumen['id_doc'], "jumlah_vektor" => $dokumen['jumlah_vektor'] ];
-            $vectorCosine[$index] = 0;
-
-            foreach ($dokumenBesarVektor as $index1 => $dokumen1) {
-                if ($dokumen1['id_doc'] == $dokumen['id_doc']) {
-                    $vectorCosine[$index] = $dokumen1["jumlah_besar_vektor"];
-
-                    $arrayDoc[$index] += ["jumlah_besar_vektor" => $vectorCosine[$index] ];
-                    array_push($dokumenCosine,  $arrayDoc[$index]);
-                }
-            }
-        }
-
-        // membuat ranking
-        $dokumenRanking = [];
-        foreach ($dokumenCosine as $index => $dokumen) {
-            $jumlah = 0;
-            if ($dokumen["jumlah_vektor"] != 0 && $dokumen["jumlah_besar_vektor"] != 0) {
-                // ganti rumus sesuai dengan aturan jaccard
-                $jumlah = 2*($dokumen['jumlah_vektor']) / ($dokumen['jumlah_besar_vektor']);
-            }
-            $arrayDoc[$index] = ["id_doc" => $dokumen['id_doc'], "ranking" => $jumlah];
-            array_push($dokumenRanking, $arrayDoc[$index]);
-        }
-
-        if ($debug){
-            var_dump('============== JACCARD SIMILARITY ==============');
-
-            var_dump('-------- jumlah query --------');
-            print_r($queryCosJumlah);
-            var_dump('/n-------- jumlah dan akar dokumen --------');
-            print_r($dokumenCosJumlahAkar);
-            var_dump('-------- vektor dokumen (perkalian)(pembilang)--------');
-            print_r($dokumenVektor);
-            var_dump('-------- besar vektor dokumen (penyebut) --------');
-            print_r($dokumenBesarVektor);
-            var_dump('-------- cosinus dokumen --------');
-            print_r($dokumenCosine);
-            var_dump('-------- rangking dokumen (hasil) --------');
-            print_r($dokumenRanking);
-        }
-
-        return $dokumenRanking;
-    }
-
     
 
 
