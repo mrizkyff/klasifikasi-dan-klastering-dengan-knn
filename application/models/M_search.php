9b79a80f946671605ccc401438c6b1bebb3f31e3
mrizkyff <muhamadrizkyff@gmail.com>
2020-11-19 21:29:40 +0700

    model serverside, nilai reset dengan parameter

--- application/models/M_search.php
+++ application/models/M_search.php
@@ -35,14 +35,69 @@
             return $this->db->get("ta_a11");
         }
         
-        public function resetAllBobot(){
+        public function resetAllBobot($value = 0){
             $data = [
-                'cosim' => 0,
-                'jaccard' => 0,
-                'dice' => 0,
+                'cosim' => $value,
+                'jaccard' => $value,
+                'dice' => $value,
             ];
             $this->db->update('ta_a11', $data);
         }
+        function json() {
+			// jangan pakai bintang nanti tidak bisa search
+			$this->datatables->select('id, penulis, tahun, judul, file, nim, cosim');
+            $this->datatables->from('ta_a11');
+            $this->datatables->where('cosim != ', '0');
+            $this->datatables->add_column('koleksi_ta',
+            '<table border="0" width="100%" style="background-color: #fed9c9" class="mt-1">
+                <tr>
+                    <td rowspan="5" width="100px">
+                        <div style="width: 80px; height: 120px; background-color: #ACDDDE"></div>
+                    </td>
+                    <td>
+                        <a href="#">$1</a>
+                        <p class="cosim">$6%</p>
+                        <script>
+                            var cosim = $(".cosim").text();
+                            if(parseInt(cosim) == -1){
+                                $(".cosim").hide();
+                            }
+                            else{
+                                
+                            }
+                        </script>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h6>$2 - $3</h6>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <h6>Teknik Informatika - S1</h6>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <div class="row mr-0">
+                            <div class="col">
+                                <h6>$4</h6>
+                            </div>
+                            <div class="col">
+                                <a href="upload/$5" target="_blank" rel="noopener noreferrer" class="float-right">Download File</a>
+                                <a class="float-right">&nbsp | &nbsp</a>
+                                <a href="#" class="float-right">Meta</a>
+                            </div>  
+                        </div>
+                    </td>
+                </tr>
+            </table>
+            ',
+            'judul, penulis, nim, tahun, file, cosim');
+
+			return $this->datatables->generate();
+		}
     }
     
 ?>
