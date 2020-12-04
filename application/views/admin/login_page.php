abb3afbfdfc6ae81a11eb1de98326ab4fe4a8580
mrizkyff <muhamadrizkyff@gmail.com>
2020-12-04 22:41:26 +0700

    update dengan logo perpustakaan

--- application/views/admin/login_page.php
+++ application/views/admin/login_page.php
@@ -35,25 +35,38 @@
 <body>
 <!-- awal navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-<div class="container">
-    <a class="navbar-brand" href="#">Perpustakaan Udinus</a>
+    <div class="container">
+    <a class="navbar-brand" href="#">
+        <table border="0">
+            <tr>
+                <td rowspan="2" width="85px">
+                    <img src="<?php echo base_url()?>asset/img/logo/logo_perpustakaan.png" class="rounded-circle" width="75px"> 
+                </td>
+                <td>
+                    <h4>PERPUSTAKAAN</h4>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <h6>UNIVERSITAS DIAN NUSWANTORO</h6>
+                </td>
+            </tr>
+        </table>
+    </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
+        <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link" href="<?php echo base_url()?>">Koleksi TA </a>
+        </li>
         <li class="nav-item active">
-            <a class="nav-link" href="<?php echo base_url()?>">Koleksi TA <span class="sr-only">(current)</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="<?php echo base_url('admin')?>">Administrator</a>
+            <a class="nav-link" href="<?php echo base_url('admin')?>">Administrator<span class="sr-only">(current)</span></a>
         </li>
         </ul>
     </div>
-</div>
+    </div>
 </nav>
 <!-- akhir navbar -->
 <div class="login-form">
--- application/views/template/admin/sidebar.php
+++ application/views/template/admin/sidebar.php
@@ -2,9 +2,9 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
-      <img src="<?php echo base_url()?>asset/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="<?php echo base_url()?>asset/img/logo/logo_perpustakaan.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">uSearch</span>
+      <span class="brand-text font-weight-light">Katalog Tugas Akhir</span>
     </a>
 
     <!-- Sidebar -->
@@ -12,10 +12,13 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?php echo base_url()?>asset/img/udinus.png" class="img-circle elevation-2" alt="User Image">
+          <!-- <img src="<?php echo base_url()?>asset/img/udinus.png" class="img-circle elevation-2" alt="User Image"> -->
+          <h2>
+            <i class="fa fa-user-circle text-light" aria-hidden="true"></i>
+          </h2>
         </div>
         <div class="info">
-          <a href="#" class="d-block">Administrator</a>
+          <a href="#" class="d-block">Pustakawan</a>
         </div>
       </div>
 
