54eb33d2faf0b2b5895aadbbfa4589952f4515c3
mrizkyff <muhamadrizkyff@gmail.com>
2020-12-14 16:40:55 +0700

    update jquery

--- application/views/template/admin/footer.php
+++ application/views/template/admin/footer.php
@@ -17,7 +17,8 @@
 
 <!-- jQuery -->
 <!-- <script src="<?php echo base_url()?>asset/plugins/jquery/jquery.min.js"></script> -->
-<script src="https://code.jquery.com/jquery-3.5.1.js" ></script>
+<!-- <script src="https://code.jquery.com/jquery-3.5.1.js" ></script> -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="<?php echo base_url()?>asset/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- DataTables -->
