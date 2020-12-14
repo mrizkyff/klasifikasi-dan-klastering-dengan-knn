3d9b425c428393c3af22bd8aec09871be1df9f93
mrizkyff <muhamadrizkyff@gmail.com>
2020-12-14 16:36:55 +0700

    update versi jquery

--- application/views/admin/login_page.php
+++ application/views/admin/login_page.php
@@ -91,7 +91,8 @@
             <label class="pull-left checkbox-inline"><input type="checkbox"> Remember me</label>
         </div>        
 </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
     <script type="text/javascript">
     $(document).ready(function(){
